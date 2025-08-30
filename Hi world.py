19b60f22904a921fac031b3f0a1cf5678fbfaeef
Gip33 <gip33.leap@gmail.com>
2025-08-30 16:45:19 -0400

    Clase 1

---
+++ Hi world.py
@@ -0,0 +1,13 @@
+hi_world = "Hola, mundo"
+
+print(hi_world)
+
+str = "Luis"
+int = 17
+float = 1.65
+boolean = None
+
+print(f"\nMi nombre es {str} y tengo {int} años.\n")
+# print(f"\n Tengo {int} años.")
+print(f"\n Mido {float} metros.")
+# print(boolean)
