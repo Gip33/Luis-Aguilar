c62c80d7a7781d6751035be4bdef5de1ae218c9d
Gip33 <151585426+Gip33@users.noreply.github.com>
2025-09-27 17:01:52 -0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,60 @@
+/*console.clear();
+//Ejercicio 1
+const name = prompt("Enter your name: ");
+var birth = prompt("Enter the year you were born in: ");
+var year = prompt("Enter the current year: ");
+
+birth = Number(birth);
+year = Number(year);
+var age = num_year - num_birth;
+console.log(`Hi ${name}.`);
+console.log(`You are ${age} years old.`)
+*/
+
+/*console.clear();
+//Ejercicio 2
+
+var age = prompt("Type your age: ");
+age = Number(age);
+if(age < 18){
+    console.log("You are underage.")
+}else{
+    console.log("You are an adult.")
+};
+*/
+
+/*console.clear();
+//Ejercicio 3
+
+var name = prompt("Type your name: ");
+var last_name = prompt("Type your last name: ");
+var full_name = name + " " + last_name;
+console.log(`Your full name is ${full_name}.`);
+*/
+
+/*console.clear();
+//Ejercicio 5
+
+console.log("Lista del 1 al 10: \n");
+
+for(i = 1; i <= 10; i++){
+    console.log(`${i}`);
+};
+*/
+
+/*console.clear();
+//Ejercicio 6
+
+var note = prompt("Type your note: ");
+if (note >= 90  && note <= 100){
+    console.log("You get an A.")
+}else if(note >= 80 && note <= 90){
+    console.log("You get an B.")
+}else if(note >= 70 && note <= 80){
+    console.log("You get an C.")
+}else if(note >= 60 && note <= 70){
+    console.log("You get an D.")
+}else{
+    console.log("You failed.")
+}
+    */
